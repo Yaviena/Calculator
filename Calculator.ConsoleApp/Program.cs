2277c8af5a77c78ae8c8efc998fe100ae20b4c09
Magda Szafranska <magdalena.szafranska@outlook.com>
2021-12-09 12:08:09 +0100

    Add headers to do in comments

--- Calculator.ConsoleApp/Program.cs
+++ Calculator.ConsoleApp/Program.cs
@@ -6,7 +6,17 @@ namespace Calculator.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 1. wyświetlenie nagłówka - opis aplikacji
+            // 2. Prośba o podanie pierwszej liczby
+            // 3. Pobranie liczby od użytkownika
+            // 4. Prośba o podanie drugiej liczby
+            // 5. Pobranie drugiej liczby od użytkownika
+            // 6. Prośba o podanie działania
+            // 7. Pobranie wybranego działania od użytkownika
+            // 8. Wykonanie obliczeń
+            // 9. Wyświetlenie wyniku użytkownikowi
+
+            Console.ReadLine();
         }
     }
 }
