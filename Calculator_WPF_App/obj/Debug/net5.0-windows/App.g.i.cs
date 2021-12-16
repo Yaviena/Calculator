96e037af74024eddbd467d27f90b5651839696f5
Magda Szafranska <magdalena.szafranska@outlook.com>
2021-12-16 09:49:32 +0100

    Add new project Win Forms and design the form window

--- Calculator_WPF_App/obj/Debug/net5.0-windows/App.g.i.cs
+++ Calculator_WPF_App/obj/Debug/net5.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace Calculator_WPF_App {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Calculator_WPF_App;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Calculator_WPF_App;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- Calculator_WPF_App/obj/Debug/net5.0-windows/Views/MainWindow.g.i.cs
+++ Calculator_WPF_App/obj/Debug/net5.0-windows/Views/MainWindow.g.i.cs
@@ -54,7 +54,7 @@ namespace Calculator_WPF_App {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Calculator_WPF_App;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Calculator_WPF_App;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
