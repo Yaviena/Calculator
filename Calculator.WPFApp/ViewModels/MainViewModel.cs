deea938610d180a4406b6e1788aed56eaf63f657
Magda Szafranska <magdalena.szafranska@outlook.com>
2021-12-28 18:17:03 +0100

    Add properties and events to the View

---
+++ Calculator.WPFApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,65 @@
+﻿using Calculator.WPFApp.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Calculator.WPFApp.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _screenVal;
+
+        public MainViewModel()
+        {
+            ScreenVal = "0";
+            AddNumberCommand = new RelayCommand(AddNumber);
+            AddOperationCommand = new RelayCommand(AddOperation);
+            ClearScreenCommand = new RelayCommand(ClearScreen);
+            GetResultCommand = new RelayCommand(ClearScreen);
+        }
+
+        private void ClearScreen(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddOperation(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddNumber(object obj)
+        {
+            MessageBox.Show(obj as string);
+        }
+
+        public ICommand AddNumberCommand { get; set; }
+        public ICommand AddOperationCommand { get; set; }
+        public ICommand ClearScreenCommand { get; set; }
+        public ICommand GetResultCommand { get; set; }
+
+        public string ScreenVal
+        {
+            get { return _screenVal; }
+            set
+            {
+                _screenVal = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- Calculator.WPFApp/Views/MainWindow.xaml.cs
+++ Calculator.WPFApp/Views/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Calculator.WPFApp.ViewModels;
 using MahApps.Metro.Controls;
 
 namespace Calculator.WPFApp
@@ -24,6 +25,7 @@ namespace Calculator.WPFApp
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
